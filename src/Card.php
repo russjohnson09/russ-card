d65552d6b23700219ec25431670f2e8488d0389b
russ <russjohnson09@gmail.com>
2015-01-21 21:00:54 -0500

    add card

---
+++ src/Card.php
@@ -0,0 +1,21 @@
+<?php namespace Russ\Cards\Models;
+
+use Illuminate\Database\Eloquent\Model;
+	
+class Card extends Model
+{
+	public function reviews()
+	{
+		return $this->hasMany('CardReview');
+	}
+	
+	public function getDueAttribute()
+	{
+		$pastReview = CardReview::where('card_id',$this->id)->orderBy('created_at','desc')->first();
+		if (empty($pastReview)) {
+			return true;
+		}
+		return $pastReview->due_date->isPast();
+		//return $pastReview->due_date->lt(Carbon::now());
+	}
+}
